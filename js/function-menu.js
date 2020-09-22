62bbb9a594fc32e662cc1f0569d92347a56f0a83
gabrielsant1 <Gabrielrezin001@gmail.com>
2020-09-22 20:06:33 -0300

    Commit - adiciona-alunos/btts funcionando 16-09

--- js/function-menu.js
+++ js/function-menu.js
@@ -2,6 +2,16 @@ $(document).ready(function() {
 
     //Abrir a página adiciona-disciplinas dentro do index.html
 
+    $('#adiciona-aluno').click(function(e) {
+        e.preventDefault() //não fazer oq está acostumado
+        $('#conteudo').empty()
+        $('#conteudo').load('src/alunos/visao/adiciona-alunos.html')
+    })
+    $('#listar-alunos').click(function(e) {
+        e.preventDefault() //não fazer oq está acostumado
+        $('#conteudo').empty()
+        $('#conteudo').load('src/alunos/visao/list-alunos.html')
+    })
     $('#adiciona').click(function(e) {
         e.preventDefault() //não fazer oq está acostumado
         $('#conteudo').empty()
